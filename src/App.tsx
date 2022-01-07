a92092db7970bc6fbe6f66347624f3f3d1f75b56
Narendirakumarkumar <naren.be2009@gmail.com>
2022-01-07 13:20:28 +0530

    Second push

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {User} from  './usedObjects';
+
 
 function App() {
+
+  let userDetails: User = {
+    name: "Naren",
+    id: 25
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        
-      </header>
+      <h1>User name is ${userDetails.name} and id is ${userDetails.id}</h1>
     </div>
   );
 }
